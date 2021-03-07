f2b10e0ed071dee50f771e30c8b605f53d430699
Jasio182 <jan.mokracki@wp.pl>
2021-03-07 16:11:30 +0100

    Created business model project

---
+++ StringManager/GuitarManager.ApplicationServices/API/Domain/GetSoundsRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuitarManager.ApplicationServices.API.Domain
+{
+    class GetSoundsRequest
+    {
+    }
+}
---
+++ StringManager/GuitarManager.ApplicationServices/API/Domain/Models/Sound.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuitarManager.ApplicationServices.API.Domain.Models
+{
+    class Sound
+    {
+    }
+}
