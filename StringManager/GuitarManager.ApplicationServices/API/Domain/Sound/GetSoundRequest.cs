ca5eb1bf07c976f417ca99870ec4ea7be7dcb233
Jasio182 <jan.mokracki@wp.pl>
2021-03-11 13:53:23 +0100

    GetSoundByID parameters fix

--- StringManager/GuitarManager.ApplicationServices/API/Domain/Sound/GetSoundRequest.cs
+++ StringManager/GuitarManager.ApplicationServices/API/Domain/Sound/GetSoundRequest.cs
@@ -4,7 +4,6 @@ namespace GuitarManager.ApplicationServices.API.Domain.Sound
 {
     public class GetSoundRequest : IRequest<GetSoundResponse>
     {
-        //public int soundId;
-        public int soundId { get; set; }
+        public int soundId;
     }
 }
